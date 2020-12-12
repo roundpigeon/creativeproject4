ce8f189e20f7aaf3072f7469d561c6e84a5c1ab2
Rachel Peterson <rachelpetersonofficial@gmail.com>
2020-12-11 23:56:53 -0700

    first commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+<div id="app">
+  <div class="header">
+    <div id="spider-icon">
+      <router-link to="/">
+        <img src="/spider-icon.png" width="50px" height="50px" />
+      </router-link>
+    </div>
+    <div class="title">
+      <router-link to="/">
+
+        <h1>Upload Some Bugs!</h1>
+      </router-link>
+    </div>
+    <div id="upload-button">
+      <router-link to="/admin"><img src="/upload-icon.png" width="50px" /></router-link>
+    </div>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
+  <a href="https://github.com/roundpigeon/creativeproject4" id="github">Github</a>
+</div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  background: #fff;
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Header */
+.header {
+  display: flex;
+  /* padding: 10px 100px 0px 100px; */
+  background-color: #2bcc6b;
+}
+
+.title {
+  margin: 5px auto 0 0;
+  text-align: center;
+}
+
+.title h1 {
+  font-size: 2.5rem;
+}
+
+.content {
+  padding: 20px 100px;
+  min-height: 500px;
+}
+
+a, a:visited {
+  text-decoration: none;
+  color: black;
+}
+
+#upload-button {
+  display: flex;
+  align-items: center;
+  margin-right: 3rem;
+}
+
+#spider-icon {
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  margin-right: 1em;
+}
+
+#github {
+  margin-left: 6.4em;
+  text-decoration: underline;
+}
+</style>
---
+++ front-end/src/views/Admin.vue
@@ -0,0 +1,197 @@
+<template>
+<div class="admin">
+ <div class="heading">
+   <h2>Upload your bug</h2>
+ </div>
+ <div class="add">
+   <div class="form">
+     <input class="new-bug" v-model="title" placeholder="Name"> <br>
+     <input class="new-bug" v-model="title" placeholder="Scientific Name"> <br>
+     <textarea class="new-bug" v-model="description" id="description" name="description" placeholder="Description" rows="4" cols="25"></textarea>
+     <p></p>
+     <input type="file" name="photo" @change="fileChanged">
+     <button @click="upload">Upload</button>
+   </div>
+   <div class="upload" v-if="addItem">
+     <h2>{{addItem.title}}</h2>
+     <p>{{addItem.scientificName}}</p>
+     <p>{{addItem.description}}</p>
+     <img :src="addItem.path" />
+   </div>
+ </div>
+ <div class="heading">
+      <h2>Remove or delete a bug</h2>
+    </div>
+    <div class="edit">
+      <div class="form">
+        <input v-model="findTitle" placeholder="Search">
+        <div class="suggestions" v-if="suggestions.length > 0">
+          <div class="suggestion" v-for="s in suggestions" :key="s.id" @click="selectItem(s)">{{s.title}}
+          </div>
+        </div>
+      </div>
+      <div class="upload" v-if="findItem">
+        <input v-model="findItem.title" placeholder="Name"> <br>
+        <input class="new-bug" v-model="findItem.scientificName" placeholder="Scientific Name"> <br>
+        <textarea class="new-bug" v-model="findItem.description" placeholder="Description" rows="4" cols="25"></textarea>
+        <p></p>
+        <img :src="findItem.path" />
+      </div>
+      <div class="actions" v-if="findItem">
+        <button @click="deleteItem(findItem)">Delete</button>
+        <button @click="editItem(findItem)">Edit</button>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Admin',
+  data() {
+    return {
+      title: "",
+      scientificName: "",
+      description: "",
+      file: null,
+      addItem: null,
+      items: [],
+      findTitle: "",
+      findItem: null,
+    }
+  },
+  computed: {
+    suggestions() {
+      let items = this.items.filter(item => item.title.toLowerCase().startsWith(this.findTitle.toLowerCase()));
+      return items.sort((a, b) => a.title > b.title);
+    }
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    fileChanged(event) {
+      this.file = event.target.files[0]
+    },
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name)
+        let r1 = await axios.post('/api/photos', formData);
+        let r2 = await axios.post('/api/items', {
+          title: this.title,
+          description: this.description,
+          scientificName: this.scientificName,
+          path: r1.data.path
+        });
+        this.addItem = r2.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    selectItem(item) {
+      this.findTitle = "";
+      this.findItem = item;
+    },
+    async deleteItem(item) {
+      try {
+        await axios.delete("/api/items/" + item._id);
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async editItem(item) {
+      try {
+        await axios.put("/api/items/" + item._id, {
+          title: this.findItem.title,
+          scientificName: this.findItem.scientificName,
+          description: this.findItem.description,
+        });
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.new-bug {
+  margin-top: 1.5em;
+}
+
+.image h2 {
+  font-style: italic;
+  font-size: 1em;
+}
+
+.heading {
+  display: flex;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+.heading h2 {
+  margin-top: 8px;
+  margin-left: 10px;
+}
+
+.add,
+.edit {
+  display: flex;
+}
+
+/* Form */
+input,
+textarea,
+select,
+button {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1em;
+}
+
+.form {
+  margin-right: 50px;
+}
+
+/* Uploaded images */
+.upload h2 {
+  margin: 0px;
+}
+
+.upload img {
+  max-width: 300px;
+}
+
+/* Suggestions */
+.suggestions {
+  width: 200px;
+  border: 1px solid #ccc;
+}
+
+.suggestion {
+  min-height: 20px;
+}
+
+.suggestion:hover {
+  background-color: #5BDEFF;
+  color: #fff;
+}
+</style>
